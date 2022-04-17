6550adb2ff8c4b018fc999a8bca390fb76ffc941
Лауркан Кушу <laurkankusu@Air-Laurkan.netis.cc>
2022-04-17 17:20:15 +0300

    authenticate method done

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -2,28 +2,44 @@ package auth
 
 import (
 	"context"
-	"landing_admin_backend/internal/domain"
 	"landing_admin_backend/internal/repository"
+	"landing_admin_backend/pkg/token_manager"
+)
+
+const (
+	TokenCookieKey   = "token"
+	RefreshCookieKey = "refresh_token"
 )
 
 type Service interface {
-	Authenticate(ctx context.Context, login string, pass string) (tokens domain.AuthTokens, err error)
+	Authenticate(ctx context.Context, login string, pass string) (tokens token_manager.AuthTokens, err error)
 }
 
 type service struct {
-	repository *repository.Repository
+	repository   *repository.Repository
+	tokenManager token_manager.TokenManager
 }
 
-func NewService(repository *repository.Repository) Service {
+func NewService(repository *repository.Repository, tokenManager token_manager.TokenManager) Service {
 	return &service{
-		repository: repository,
+		repository:   repository,
+		tokenManager: tokenManager,
 	}
 }
 
-func (s *service) Authenticate(ctx context.Context, login string, pass string) (tokens domain.AuthTokens, err error) {
+func (s *service) Authenticate(ctx context.Context, login string, pass string) (tokens token_manager.AuthTokens, err error) {
 	user, err := s.repository.Users.GetUserByCredetinals(ctx, login, pass)
 	if err != nil {
 		return
 	}
+	tokens, err = s.tokenManager.GenerateAuthTokens(user.ID.Hex(), user.Name, user.Role)
+	if err != nil {
+		return
+	}
+	err = setTokensToCookies(tokens)
+	return
+}
 
+func setTokensToCookies(tokens token_manager.AuthTokens) (err error) {
+	return
 }
