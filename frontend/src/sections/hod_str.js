d394cb0cfceee6de069272190f470c28cd5c9210
ratamanov <r399055750@gmail.com>
2022-06-09 16:57:11 +0500

    fix slider

--- frontend/src/sections/hod_str.js
+++ frontend/src/sections/hod_str.js
@@ -11,9 +11,21 @@ export const HodStr = () => {
     dots: false,
     variableWidth: true,
     centerMode: true,
-    fade: false,
     arrows: true,
-    centerPadding: 120
+    infinite: true,
+    centerPadding: 0,
+    responsive: [
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          arrows: false,
+          centerPadding: 10,
+        }
+      },
+    ]
   }
 
   let settings2 = {
@@ -65,30 +77,28 @@ export const HodStr = () => {
           <div class="construction-progress__slider">
             <div class="slider-construction-progress js-slider-construction-progress-wrapper">
               <Slider asNavFor={nav2} ref={(slider1) => setNav1(slider1)} className="slider slider-construction-progress__slider-for js-slider-construction-progress__slider-for" {...settings}>
-                <div class="slide"><img src="img/constr-prog1.png" alt="" /></div>
-                <div class="slide"><img src="img/constr-prog1.png" alt="" /></div>
-                <div class="slide"><img src="img/constr-prog1.png" alt="" /></div>
-                <div class="slide"><img src="img/constr-prog1.png" alt="" /></div>
-                <div class="slide"><img src="img/constr-prog1.png" alt="" /></div>
-                <div class="slide"><img src="img/constr-prog1.png" alt="" /></div>
-                <div class="slide"><img src="img/constr-prog1.png" alt="" /></div>
-                <div class="slide"><img src="img/constr-prog1.png" alt="" /></div>
+                <div className="slide"><img src="img/constr-prog1.png" alt=""/></div>
+                <div className="slide"><img src="img/constr-prog1.png" alt=""/></div>
+                <div className="slide"><img src="img/constr-prog1.png" alt=""/></div>
+                <div className="slide"><img src="img/constr-prog1.png" alt=""/></div>
+
               </Slider>
               <div class="slider-construction-progress-nav">
-                <div class="arr-left" onClick={()=> nav1.slickPrev()}>
+                <div class="arr-left" onClick={()=> nav2.slickPrev()}>
                   <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1.5 10.5L0.792893 9.79289L0.0857865 10.5L0.792893 11.2071L1.5 10.5ZM8.20711 15.7929L2.20711 9.79289L0.792893 11.2071L6.79289 17.2071L8.20711 15.7929ZM2.20711 11.2071L8.20711 5.20711L6.79289 3.79289L0.792893 9.79289L2.20711 11.2071ZM1.5 11.5L18.5 11.5V9.5L1.5 9.5L1.5 11.5Z" />
                   </svg>
                 </div>
                 <div class="nav-build">
                   <Slider asNavFor={nav1} ref={(slider2) => setNav2(slider2)} className={"slider-construction-progress__slider-nav js-slider-construction-progress__slider-nav"} {...settings2}>
-                    <div class="slider-nav-item"><span>01</span></div>
-                    <div class="slider-nav-item"><span>02</span></div>
-                    <div class="slider-nav-item"><span>03</span></div>
-                    <div class="slider-nav-item"><span>04</span></div>
+
+                    <div className="slider-nav-item"><span>01</span></div>
+                    <div className="slider-nav-item"><span>02</span></div>
+                    <div className="slider-nav-item"><span>03</span></div>
+                    <div className="slider-nav-item"><span>04</span></div>
                   </Slider>
                 </div>
-                <div class="arr-right" onClick={()=> nav1.slickNext()}>
+                <div class="arr-right" onClick={()=> nav2.slickNext()}>
                   <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M18.5 9.5L19.2071 10.2071L19.9142 9.5L19.2071 8.79289L18.5 9.5ZM11.7929 4.20711L17.7929 10.2071L19.2071 8.79289L13.2071 2.79289L11.7929 4.20711ZM17.7929 8.79289L11.7929 14.7929L13.2071 16.2071L19.2071 10.2071L17.7929 8.79289ZM18.5 8.5L1.5 8.5L1.5 10.5L18.5 10.5V8.5Z" />
                   </svg>
