39fd65886d6dc398f2dd7c67f149f665dc0294de
Laurkan Kushu <kanya384@mail.ru>
2022-06-27 23:59:42 +0300

    added metrics

---
+++ frontend/src/paths.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+
+
+fs.readFile('./build/index.html', 'utf8', (err, data) => {
+  //data = data.replaceAll('"/static', '"static')
+  let script = ""
+  let style = ""
+  let i = data.indexOf('script defer="defer" src="') + 'script defer="defer" src="'.length;
+  while (data[i] != '"') {
+    script += data[i]
+    i++
+  }
+  i = data.indexOf('link href="', i) + 'link href="'.length;
+  while (data[i] != '"') {
+    style += data[i]
+    i++
+  }
+  fs.readFile("./build/"+script, "utf-8", (err, scriptT) => {
+    fs.readFile("./build/"+style, "utf8", (err, styleT) => {
+      //data = data.replace('<script defer="defer" src="' + script + '"></script>', '<script type="text/javascript">' + scriptT.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0') + '</script>')
+      data = data.replace('<link href="' + style + '" rel="stylesheet">', '<style>' + styleT + '</style>')
+    
+    
+      fs.readFile("./src/scripts.js", "utf8", (err, scripts) => {
+        data = data.replaceAll("</body>", "</body>\n<script>\n"+scripts+"\n</script>");
+        fs.writeFileSync("./build/index.html", data)
+      });
+    })
+  })
+  
+})
+
---
+++ frontend/src/scripts.js
@@ -0,0 +1,28 @@
+setTimeout(()=>{
+  (function(w, d, s, h, id) {
+    w.roistatProjectId = id; w.roistatHost = h;
+    var p = d.location.protocol == "https:" ? "https://" : "http://";
+    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
+    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+  })(window, document, 'script', 'cloud.roistat.com', '99fa8918141b4540aed4ab615fea5a7a');
+},1000)
+setTimeout(()=>{
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(87671212, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true,
+        ecommerce:"dataLayer"
+  });
+
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-MN38KG8');
+
+}, 3000)
